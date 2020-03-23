55cae09fe54a5f9ae65501256bc26bf044ab8bcf
J J <44272212+janmejaysyngh@users.noreply.github.com>
2020-03-23 19:28:15 +0530

    Add files via upload

---
+++ Lesson_2_LED_trailing_effects.ino
@@ -0,0 +1,75 @@
+
+/* Arduino Starter Kit-CoreChamp by Janmejay Singh
+    Lesson 2: LED trailing effects
+    
+ Turn an LED on for one second off for one second and repeat forever.
+ 
+ This code is completely free for any use.
+ Visit https://github.com/janmejaysyngh/Arduino-Starter-Kit-CoreChamp .
+ Visit http://www.corechamp.in for more information.
+ Visit http://www.arduino.cc to learn about the Arduino.
+*/
+
+
+void setup() {
+  
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+ 
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  delay(50);
+  digitalWrite(2, LOW);
+  delay(50);
+  digitalWrite(3, HIGH);
+  delay(50);
+  digitalWrite(3, LOW);
+  delay(50);
+  digitalWrite(4, HIGH);
+  delay(50);
+  digitalWrite(4, LOW);
+  delay(50);
+  digitalWrite(5, HIGH);
+  delay(50);
+  digitalWrite(5, LOW);
+  delay(50);
+  digitalWrite(6, HIGH);
+  delay(50);
+  digitalWrite(6, LOW);
+  delay(50);
+  digitalWrite(7, HIGH);
+  delay(50);
+  digitalWrite(7, LOW);
+  delay(50);
+  digitalWrite(7, HIGH);
+  delay(50);
+  digitalWrite(7, LOW);
+  delay(50);
+  digitalWrite(6, HIGH);
+  delay(50);
+  digitalWrite(6, LOW);
+  delay(50);
+  digitalWrite(5, HIGH);
+  delay(50);
+  digitalWrite(5, LOW);
+  delay(50);
+  digitalWrite(4, HIGH);
+  delay(50);
+  digitalWrite(4, LOW);
+  delay(50);
+  digitalWrite(3, HIGH);
+  delay(50);
+  digitalWrite(3, LOW);
+  delay(50);
+  digitalWrite(2, HIGH);
+  delay(50);
+  digitalWrite(2, LOW);
+  delay(50);
+  
+}
