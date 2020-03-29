e116fe1e6cf9c03b429c82336cd413d7987ce30a
J J <44272212+janmejaysyngh@users.noreply.github.com>
2020-03-29 23:50:07 +0530

    Lesson_6_Button

---
+++ Lesson_6_Button.ino
@@ -0,0 +1,41 @@
+/* Arduino Starter Kit-CoreChamp by Janmejay Singh
+    Lesson 6:Button
+
+ Turns on and off a light emitting diode(LED) connected to digital
+ pin 13, when pressing a pushbutton attached to pin 2.
+
+
+ The circuit:
+ * LED attached from pin 13 to ground
+ * pushbutton attached to pin 2 from +5V
+ * 10K resistor attached to pin 2 from ground
+
+ */
+
+const int buttonPin = 2;     // the number of the button pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the button status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the button value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the button is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
