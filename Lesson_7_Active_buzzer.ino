5f960e621a8a7304b3c3e48e667d4898fc584969
J J <44272212+janmejaysyngh@users.noreply.github.com>
2020-03-30 01:32:31 +0530

    Update Lesson_7_Active_buzzer.ino

--- Lesson_7_Active_buzzer.ino
+++ Lesson_7_Active_buzzer.ino
@@ -10,6 +10,10 @@
  * buzzer '+'ve attached to pin 13
  * buzzer '-'ve attached to ground
 
+This code is completely free for any use.
+ Visit https://github.com/janmejaysyngh/Arduino-Starter-Kit-CoreChamp .
+ Visit http://www.corechamp.in for more information.
+ Visit http://www.arduino.cc to learn about the Arduino.
  */
  
 void setup() {
