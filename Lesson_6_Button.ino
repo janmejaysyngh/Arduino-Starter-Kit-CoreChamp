0777f72203467afd8f6836d26be385f197cb7f7b
J J <44272212+janmejaysyngh@users.noreply.github.com>
2020-03-30 01:31:58 +0530

    Update Lesson_6_Button.ino

--- Lesson_6_Button.ino
+++ Lesson_6_Button.ino
@@ -11,6 +11,10 @@
  * pushbutton attached to pin 2 from +5V
  * 10K resistor attached to pin 2 from ground
 
+This code is completely free for any use.
+ Visit https://github.com/janmejaysyngh/Arduino-Starter-Kit-CoreChamp .
+ Visit http://www.corechamp.in for more information.
+ Visit http://www.arduino.cc to learn about the Arduino.
  */
 
 
