421a0e278f7709e830b1c6ecfb840a82d0095876
J J <44272212+janmejaysyngh@users.noreply.github.com>
2020-03-29 23:44:05 +0530

    Add files via upload

---
+++ Lesson_4_Analog_input.ino
@@ -0,0 +1,44 @@
+
+/* Arduino Starter Kit-CoreChamp by Janmejay Singh
+    Lesson 4: Analog Input
+ Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ turning on and off a light emitting diode(LED)  connected to digital pin 13.
+ The amount of time the LED will be on and off depends on
+ the value obtained by analogRead().
+
+ The circuit:
+ * Potentiometer attached to analog input 0
+ * center pin of the potentiometer to the analog pin
+ * one side pin (either one) to ground
+ * the other side pin to +5V
+ * LED anode (long leg) attached to digital output 13
+ * LED cathode (short leg) attached to ground
+ 
+ This code is completely free for any use.
+ Visit https://github.com/janmejaysyngh/Arduino-Starter-Kit-CoreChamp .
+ Visit http://www.corechamp.in for more information.
+ Visit http://www.arduino.cc to learn about the Arduino.
+*/
+
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  // turn the ledPin on
+  digitalWrite(ledPin, HIGH);
+  // stop the program for <sensorValue> milliseconds:
+  delay(sensorValue);
+  // turn the ledPin off:
+  digitalWrite(ledPin, LOW);
+  // stop the program for for <sensorValue> milliseconds:
+  delay(sensorValue);
+}
