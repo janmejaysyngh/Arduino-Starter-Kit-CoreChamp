e8658a54e5fbda708e73ff483ba9a90437990725
J J <44272212+janmejaysyngh@users.noreply.github.com>
2020-03-30 12:53:30 +0530

    Lesson_15_Flame_alarm_system.ino

---
+++ Lesson_15_Flame_alarm_system.ino
@@ -0,0 +1,48 @@
+/*
+Arduino Starter Kit-CoreChamp by Janmejay Singh
+    Lesson 15: Flame alarm system
+Flame sensor    UNO R3
+Short Pin ->  +5V
+Long Pin  ->  A0
+Buzzer    ->  D6
+Note：The short pin of the Flame sensor is connected to +5v.
+
+ This code is completely free for any use.
+ Visit https://github.com/janmejaysyngh/Arduino-Starter-Kit-CoreChamp .
+ Visit http://www.corechamp.in for more information.
+ Visit http://www.arduino.cc to learn about the Arduino.
+ */
+ 
+int buzzer = 6;
+int LED = 7;
+int flame_sensor = 4;
+int flame_detected;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(flame_sensor, INPUT);
+}
+
+void loop()
+{
+  flame_detected = digitalRead(flame_sensor);
+  if (flame_detected == 1)
+  {
+    Serial.println("Flame detected...! take action immediately.");
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(LED, HIGH);
+    delay(200);
+    digitalWrite(LED, LOW);
+    delay(200);
+  }
+  else
+  {
+    Serial.println("No flame detected. stay cool");
+    digitalWrite(buzzer, LOW);
+    digitalWrite(LED, LOW);
+  }
+  delay(1000);
+}
